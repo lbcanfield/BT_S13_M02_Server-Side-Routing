2c4c63d8835e294a658073f3721440643e528a65
lbcanfield <lewie.b.canfield@gmail.com>
2023-06-14 09:20:54 -0500

    /src/api/posts/posts-router.js - [GET] endpoint created to retreive all posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,6 +1,19 @@
-// implement your posts router here
 const express = require('express');
 const router = express.Router();
+const POSTS = require('./posts-model');
+
+router.get('/', (request, response) => {
+     POSTS.find()
+          .then(allPosts => {
+               response.status(200).json(allPosts)
+          })
+          .catch(error => {
+               response.status(500).json({
+                    message: "The posts information could not be retrieved"
+               })
+          })
+})
+
 
 
 
