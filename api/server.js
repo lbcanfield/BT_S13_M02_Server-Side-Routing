3bbcd6a3181d26a4ecbc7b4940b0f381b389b43c
lbcanfield <lewie.b.canfield@gmail.com>
2023-06-14 09:07:46 -0500

    /src/server.js - variable for posts-router connected
    /src/api/posts/posts-router.js
    - simple router created.

--- api/server.js
+++ api/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 
+const postsRouter = require('./posts/posts-router');
+
 const server = express();
 
 server.use(express.json());
 
-
+server.use('/api/posts', postsRouter);
 
 //Catch-all endpoint
 server.use('*', (request, response) => {
-     response.json({
+     response.status(404).json({
           message: 'Server Not Found'
      })
 });
