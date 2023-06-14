e6d33efe93b59755e7f8e182d0ced1e6903a1c5d
lbcanfield <lewie.b.canfield@gmail.com>
2023-06-14 09:32:15 -0500

    /src/api/posts/posts-router.js - [GET]:id endpoint created to extract a specific post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -14,6 +14,24 @@ router.get('/', (request, response) => {
           })
 })
 
+router.get('/:id', async (request, response) => {
+     const { id } = request.params;
+     try {
+          // throw new Error('booh');
+          const fetchedPost = await POSTS.findById(id);
+          fetchedPost ?
+               response.status(200).json(fetchedPost) :
+               response.status(404).json({
+                    message: "The post with the specified ID does not exist"
+               })
+     }
+     catch (error) {
+          response.status(500).json({
+               message: "The post information could not be retrieved"
+          })
+     }
+})
+
 
 
 
